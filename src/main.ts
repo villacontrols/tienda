7bb6050af72effc0527ba8216b8fecdc5028af45
jhon anderson <garciacalvojhonanderson@gmail.com>
2025-08-27 01:48:22 -0500

    url

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
     app.enableCors({
-    origin: ['http://localhost:5173'], // agrega aquí tu frontend
+    origin: ['http://localhost:5173', "https://prueba-usuario-flax.vercel.app/"], // agrega aquí tu frontend
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
